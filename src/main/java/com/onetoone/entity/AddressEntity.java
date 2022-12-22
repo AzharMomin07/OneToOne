d33b6ca92677808af6f477d3c15c928c61caddd7
Azhar Momin <azharmomin466@gmail.com>
2022-12-22 18:03:37 +0530

    SuccessFully Comitted

---
+++ src/main/java/com/onetoone/entity/AddressEntity.java
@@ -0,0 +1,29 @@
+package com.onetoone.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Entity(name = "address")
+@Data
+
+public class AddressEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id ;
+
+    private String address1 ;
+
+    private String address2 ;
+
+    private String state ;
+
+    private String city ;
+
+    @OneToOne(mappedBy = "address")
+    private PersonEntity person ;
+}
